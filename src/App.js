f21d20f1af551acc9c9d5628c1f7ee97221afc8f
lha901222 <lha901222@gmail.com>
2024-04-03 13:04:29 +0900

    할 일 앱 만들기 UI 구성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import TodoListItem from "./components/TodoListItem";
 import TodoTemplate from "./components/TodoTemplate";
-
+import TodoInsert from "./components/Todoinsert";
 
 function App() {
-  return <TodoTemplate></TodoTemplate>;
+  // const [value, setValue] = useState("");
+
+  // const onChange = event => {
+  //   setValue(event.target.value);
+  // };
+
+  // const onSubmit = (event)=>{
+  //   // submit 이벤트는 브라우저에서 새로고침 발생시킴
+  //   // 이를 방지하기 위해 아래 함수 호출
+  //   event.preventDefault();
+  //   setValue("");
+  // }
+
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoListItem />
+    </TodoTemplate>
+  );
 }
 
 export default App;
