7ae67adff5cc11000218af11311bc34941176699
lha901222 <lha901222@gmail.com>
2024-04-04 13:07:50 +0900

    할 일 앱 만들기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
+import { useRef, useState } from "react";
 import TodoList from "./components/TodoList";
 import TodoListItem from "./components/TodoListItem";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/Todoinsert";
 
+const initState = [
+  {
+    // id: 고유의 (식별)번호
+    // 같은 text라면 식별해주기 위해
+    id: 1,
+    text: "리액트의 기초 알아보기",
+    checked: true,
+  },
+  {
+    id: 2,
+    text: "컴포넌트의 이해",
+    checked: false,
+  },
+  {
+    id: 3,
+    text: "할 일 앱 만들어 보기",
+    checked: true,
+  },
+];
+
 function App() {
-  // const [value, setValue] = useState("");
+  const [todos, setTodos] = useState(initState);
 
-  // const onChange = event => {
-  //   setValue(event.target.value);
-  // };
+  // id는 고유한 값이여야 한다.
+  const nextId = useRef(4);
 
-  // const onSubmit = (event)=>{
-  //   // submit 이벤트는 브라우저에서 새로고침 발생시킴
-  //   // 이를 방지하기 위해 아래 함수 호출
-  //   event.preventDefault();
-  //   setValue("");
-  // }
+  // todos 배열에 새 객채 추가
+  const onInsert = text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  };
+
+  // 삭제 기능
+  const onRemove = id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  // 토글 기능
+  const onToggle = id => {
+    setTodos(
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+      ),
+    );
+  };
 
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
